55336a86096429afae3c72b8e976ab1d59de26ba
guillaumeingrin <guillaume.ingrin@gmail.com>
2023-11-23 17:19:19 +0100

    done

--- app/models/console.rb
+++ app/models/console.rb
@@ -1,7 +1,7 @@
 class Console < ApplicationRecord
   belongs_to :user
-  has_many :bookings
-  has_many :reviews
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :price, presence: true
   validates :brand, presence: true
